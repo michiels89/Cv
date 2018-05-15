b1ff1ff1f629ad40fb939c1f86878708bd6bce7e
lynn <michiels89@hotmail.com>
2018-05-15 15:08:46 +0200

    favicon added & dropdownmenu working

--- js/scripts.js
+++ js/scripts.js
@@ -35,15 +35,15 @@ jQuery(function ($) {
     // Animated scrolling / Scroll Up
     // -------------------------------------------------------------
 
-    (function () {
-        $('a[href*=#]').bind("click", function(e){
-            var anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top
-            }, 1000);
-            e.preventDefault();
-        });
-    }());
+//    (function () {
+//        $('a[href*=#]').bind("click", function(e){
+//            var anchor = $(this);
+//            $('html, body').stop().animate({
+//                scrollTop: $(anchor.attr('href')).offset().top
+//            }, 1000);
+//            e.preventDefault();
+//        });
+//    }());
 
 
 
